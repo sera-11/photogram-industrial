ec2c3b3101fdeb0afd02f696ec90a1d477b51f60
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-02-22 19:34:39 +0000

    enum for follow requests

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -22,4 +22,11 @@
 class FollowRequest < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :sender, class_name: "User"
+
+  enum status: { pending: "pending", rejected: "rejected", accepted: "accepted" } #enumerating
+#   # assume follow_request is a valid and pending
+# follow_request.accepted? # => false
+# follow_request.accepted! # sets status to "accepted" and saves
+
+
 end

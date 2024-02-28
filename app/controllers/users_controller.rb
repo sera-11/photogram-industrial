605cfeb8bbd7680d33b05b0ba78ab819418091fa
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-02-28 19:36:32 +0000

    rcav followings, updated nav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,8 @@ class UsersController < ApplicationController
     @user = User.find_by!(username: params.fetch(:username))
   end
 
-  # def following
-  #   @user = User.find_by!(username: params.fetch(:username))
-  # end
+  def following
+     @user = User.find_by!(username: params.fetch(:username))
+   end
   
 end
